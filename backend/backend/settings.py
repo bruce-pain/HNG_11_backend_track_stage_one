c48997617fe31615598cc847be5d30646c2d54dc
Franklin Ikeh <ikehfranklin06@gmail.com>
2024-07-01 22:51:57 +0100

    vercel settings

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-5yeu_y3e6fqvjft#i8lu5atjh9wj49_lku6xhba_95uzj_s@uh'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1", ".vercel.app"]
 
 
 # Application definition
